58ebf759d52eb3ccefa0573945c09f13db3ae8d2
Maria Fernanda <maria.mongas@kmg.com.ve>
2022-03-14 20:21:24 -0400

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/*Dependencies*/
+import React, { Fragment} from "react";
+
+
+
+/*Components */
+import Home from './components/Home';
+// import { PurchaseDetail } from "./components/PurchaseDetail";
+
+
+
+export function App() {
+ 
+
+
+  
+  return (
+    <Fragment>
+      <Home />
+       {/* <PurchaseDetail />  */}
+ 
+    </Fragment>
+  );
+}
